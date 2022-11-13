bbdd4bdb3afdcb1c71116e00142836f2b6a985b7
Oskar Reszka <oskarreszka12@gmail.com>
2022-11-13 15:11:55 +0100

    Base web app

---
+++ .NET App/Shrooms/Controllers/BlobStorageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.IO;
+
+namespace Shrooms.Controllers
+{
+    public class BlobStorageController
+    {
+        public async Task SaveFileToStorage(IBrowserFile file)
+        {
+            //zapis obrazu do blob storage
+            //await blobContainerClient.UploadBlobAsync(trustedFilename, browserFile.OpenReadStream());
+        }
+    }
+}
---
+++ .NET App/Shrooms/Controllers/CustomVisionController.cs
@@ -0,0 +1,11 @@
+﻿namespace Shrooms.Controllers
+{
+    public class CustomVisionController
+    {
+        public async Task<string> ClassifyMushroom()
+        {
+            // klasyfikacja z custom vision
+            return "";
+        }
+    }
+}
