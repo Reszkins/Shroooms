79fa2b3200616dc2a2fc93eef79de9b986f1fdf9
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-11-20 18:19:04 +0100

    added blob storage

---
+++ .NET App/Shrooms/Authentication/StorageBlobAuthenticator.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+namespace Shrooms.Authentication
+{
+    public class StorageBlobAuthenticator
+    {
+        public static BlobServiceClient AutencitateBlobStorage(string connectionString)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            return blobServiceClient;
+        }
+
+        public static BlobContainerClient GetBlobContainerClient(string connectionString, string containerName)
+        {
+            var container = new BlobContainerClient(connectionString, containerName);
+
+            return container;
+        }
+
+    }
+}
